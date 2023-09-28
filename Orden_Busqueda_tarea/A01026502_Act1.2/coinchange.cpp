4a649abecddd82881f4e5bb212f25cb04e6b4808
Fernanda Osorio <A01026502@tec.mx>
2023-09-28 14:45:54 -0600

    dynamic

--- Orden_Busqueda_tarea/A01026502_Act1.2/coinchange.cpp
+++ Orden_Busqueda_tarea/A01026502_Act1.2/coinchange.cpp
@@ -1,6 +1,6 @@
-//Fernanda Osorio Arroyo - A01026502
-// 28 de septiembre del 2023
-//Este archivo sirve para resolver el problema "cambio de monedas" utilizando el algoritmo Greedy y programación Dinámica
+// Fernanda Osorio Arroyo - A01026502
+//  28 de septiembre del 2023
+// Este archivo sirve para resolver el problema "cambio de monedas" utilizando el algoritmo Greedy y programación Dinámica
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,48 +10,69 @@ using namespace std;
 
 // Declaraciones de funciones
 vector<int> monedas;
-void minNumMonGR(int n);
-void minNumMonPD(int n);
+vector<int> moneditas;
+vector<int> minNumMonGR(int n);
+vector<int> minNumMonPD(int n);
+void escribeRespuesta(int n);
 
 int main(int argc, char *argv[])
 {
 
-    // Declaración de variables
-    vector<int> v; // vector para guardar los datos del archivo
-    vector<int> v2;
-    int n = 0; // número de monedas
-    int j = 0; // costo
-    int k = 0; // pago
+     // Declaración de variables
+     vector<int> v; // vector para guardar los datos del archivo
+     vector<int> v2;
+     int n = 0; // número de monedas
+     int j = 0; // costo
+     int k = 0; // pago
+     int x = 0; // variable para guardar el resultado de la función getChange
 
-    // readFile("ejemplo01coins.txt", v); // leer el archivo y guardar los datos en el vector v
-    cout << "\x1B[1;32m"
-         << "Cuantas monedas hay:"
-         << "\x1B[0m" << endl;
-    cin >> n;
-    cout << "\x1B[1;32m"
-         << "Posibles denomicaciones:"
-         << "\x1B[0m" << endl;
-    recibeDatos(n, monedas); // leer el archivo y guardar los datos en el vector v
-    cout << "\x1B[1;34m"
-         << "Cual es el costo:"
-         << "\x1B[0m" << endl;
-    cin >> j;
-    cout << "\x1B[1m"
-         << "Cual es el pago:"
-         << "\x1B[0m" << endl;
-    cin >> k;
-    cout << "\x1B[1;35m"
-         << "Usando Greedy Algorithm:"
-         << "\x1B[0m" << endl;
-    minNumMonGR(getChange(j, k));
+     // readFile("ejemplo01coins.txt", v); // leer el archivo y guardar los datos en el vector v
+     cout << "\x1B[1;32m"
+          << "Cuantas monedas hay:"
+          << "\x1B[0m" << endl;
+     cin >> n;
+     cout << "\x1B[1;32m"
+          << "Posibles denomicaciones:"
+          << "\x1B[0m" << endl;
+     recibeDatos(n, monedas); // leer el archivo y guardar los datos en el vector v
+     cout << "\x1B[1;34m"
+          << "Cual es el costo:"
+          << "\x1B[0m" << endl;
+     cin >> j;
+     cout << "\x1B[1m"
+          << "Cual es el pago:"
+          << "\x1B[0m" << endl;
+     cin >> k;
+     cout << "\x1B[1;35m"
+          << "Usando Greedy Algorithm:"
+          << "\x1B[0m" << endl;
+     x = getChange(k, j);
+     cout << "\x1B[1m"
+          << "El cambio es:  " << x
+          << "\x1B[0m" << endl;
+     moneditas = minNumMonGR(x);
+     escribeRespuesta(x);
+     cout << "\x1B[1;36m"
+          << "Usando Dynamic Programming:"
+          << "\x1B[0m" << endl;
+     moneditas = minNumMonPD(x);
+     escribeRespuesta(x);
+     
 
-    return 0;
+     return 0;
 }
 
-void minNumMonGR(int n){
-    minNumMonGR(n, monedas);
+vector<int> minNumMonGR(int n)
+{
+     return minNumMonGR(n, monedas);
 }
 
-void minNumMonPD(int n){
-    minNumMonPD(n, monedas);
+vector<int> minNumMonPD(int n)
+{
+     return minNumMonPD(n, monedas);
+}
+
+void escribeRespuesta(int n)
+{
+     escribeRespuesta(n, moneditas);
 }
--- Orden_Busqueda_tarea/A01026502_Act1.2/coinchange_funciones.h
+++ Orden_Busqueda_tarea/A01026502_Act1.2/coinchange_funciones.h
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
 #ifndef coinchange_H
 #define coinchange_H
 
@@ -17,8 +20,9 @@ void recibeDatos(int n, vector<int> &v);
 void printVector(vector<int> v);
 int readFile(string x, vector<int> &v);
 int getChange(int n, int j);
-int minNumMonGR(int n, vector<int> &v);
-int minNumMonPD(int n, vector<int> &v);
+vector<int> minNumMonGR(int n, vector<int> &v);
+vector<int> minNumMonPD(int n, vector<int> &v);
+void escribeRespuesta(int n, const vector<int>& v);
 
 // función para imprimir el vector, recibe un vector de enteros
 // regresa un 0 si se pudo imprimir el vector
@@ -90,11 +94,11 @@ int getChange(int n, int j)
 {
     int c;
     // cout << "\nEl cambio es: " << endl;
-    c = j - n;
+    c = n - j;
     return c;
 }
 
-int minNumMonGR(int n, vector<int> &v)
+vector<int> minNumMonGR(int n, vector<int> &v)
 {
     sort(v.begin(), v.end()); // ordenar el vector de menor a mayor
     vector<int> monedas;      // vector para guardar las monedas
@@ -107,17 +111,48 @@ int minNumMonGR(int n, vector<int> &v)
         }
     }
     // imprimir el vector de monedas
-    cout << "\nEl numero minimo de monedas es: " << monedas.size() << endl;
-    cout << "\nLas monedas son: " << endl;
-    for (int i = 0; i < monedas.size(); i++)
-    {
-        cout << monedas[i] << " ";
-    }
-    cout << endl;
+    cout << "\nEl numero minimo de monedas con greedy es: " << monedas.size() << endl;
+    return monedas;
 }
 
-int minNumMonPD(int n, vector<int> &v)
+vector<int> minNumMonPD(int n, vector<int> &v)
 {
+    sort(v.begin(), v.end()); // ordenar el vector de menor a mayor
+    vector<int> monedas;      // vector para guardar las monedas
+    vector <int> dp(n+1, INT_MAX); //INT_MAX para indicar que no hay solución
+    dp[0] = 0;
+    for (int i = 1; i <= n;i++){
+        for (int j = 0; j < v.size();j++){
+            if (v[j] <= i){
+                int sub_res = dp[i - v[j]]; //sub_res es el resultado de la subproblema
+                if (sub_res != INT_MAX && sub_res + 1 < dp[i]){ //si hay solución y es menor a la que ya se tiene
+                    dp[i] = sub_res + 1; //actualizar el valor de la solución
+                }
+            }
+        }
+    }
+    int remaining = n;
+    while (remaining > 0){
+        for (int i = 0; i < v.size();i++){
+            if (dp[remaining] == dp[remaining - v[i]] + 1){
+                monedas.push_back(v[i]);
+                remaining = remaining - v[i];
+                break;
+            }
+        }
+    }
+    cout << "\nEl numero minimo de monedas con programación dinámica es: " << monedas.size() << endl;
+    return monedas;
+
+}
+
+void escribeRespuesta(int n, const vector<int>& v){
+    cout << "\nLas monedas son: " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
 }
 
 #endif
