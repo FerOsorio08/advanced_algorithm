b43348e51ff06c02276fb65fe0fd37b3f9d36cd9
Fernanda Osorio <A01026502@tec.mx>
2023-09-28 22:19:54 -0600

    escribe respuestas change to map

--- Orden_Busqueda_tarea/A01026502_Act1.2/coinchange.cpp
+++ Orden_Busqueda_tarea/A01026502_Act1.2/coinchange.cpp
@@ -11,68 +11,61 @@ using namespace std;
 // Declaraciones de funciones
 vector<int> monedas;
 vector<int> moneditas;
-vector<int> minNumMonGR(int n);
-vector<int> minNumMonPD(int n);
-void escribeRespuesta(int n);
+vector<int> minNumMonGR(int n); // función para calcular el cambio en monedas con el algoritmo Greedy
+vector<int> minNumMonPD(int n); // función para calcular el cambio en monedas con programación dinámica
+void escribeRespuesta(int n);   // función para imprimir el cambio en monedas
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 
      // Declaración de variables
      vector<int> v; // vector para guardar los datos del archivo
-     vector<int> v2;
-     int n = 0; // número de monedas
-     int j = 0; // costo
-     int k = 0; // pago
-     int x = 0; // variable para guardar el resultado de la función getChange
+     int n = 0;     // número de monedas
+     int j = 0;     // costo
+     int k = 0;     // pago
+     int x = 0;     // variable para guardar el resultado de la función getChange
 
-     // readFile("ejemplo01coins.txt", v); // leer el archivo y guardar los datos en el vector v
      cout << "\x1B[1;32m"
           << "Cuantas monedas hay:"
           << "\x1B[0m" << endl;
-     cin >> n;
+     cin >> n; // recibe el número de monedas
      cout << "\x1B[1;32m"
           << "Posibles denomicaciones:"
           << "\x1B[0m" << endl;
-     recibeDatos(n, monedas); // leer el archivo y guardar los datos en el vector v
+     recibeDatos(n, monedas); // leer el archivo y guardar las denominaciones en el vector v
      cout << "\x1B[1;34m"
           << "Cual es el costo:"
           << "\x1B[0m" << endl;
-     cin >> j;
+     cin >> j; // recibe el costo
      cout << "\x1B[1m"
           << "Cual es el pago:"
           << "\x1B[0m" << endl;
-     cin >> k;
+     cin >> k;            // recibe el pago
+     x = getChange(k, j); // calcuka el cambio con función getChange
      cout << "\x1B[1;35m"
           << "Usando Greedy Algorithm:"
           << "\x1B[0m" << endl;
-     x = getChange(k, j);
      cout << "\x1B[1m"
           << "El cambio es:  " << x
           << "\x1B[0m" << endl;
-     moneditas = minNumMonGR(x);
+     moneditas = minNumMonGR(x); // guarda el resultado de la función minNumMonGR en el vector moneditas
      escribeRespuesta(x);
      cout << "\x1B[1;36m"
           << "Usando Dynamic Programming:"
           << "\x1B[0m" << endl;
-     moneditas = minNumMonPD(x);
+     moneditas = minNumMonPD(x); // guarda el resultado de la función minNumMonPD en el vector moneditas
      escribeRespuesta(x);
-     
 
      return 0;
 }
 
-vector<int> minNumMonGR(int n)
-{
+vector<int> minNumMonGR(int n){
      return minNumMonGR(n, monedas);
 }
 
-vector<int> minNumMonPD(int n)
-{
+vector<int> minNumMonPD(int n){
      return minNumMonPD(n, monedas);
 }
 
-void escribeRespuesta(int n)
-{
+void escribeRespuesta(int n){
      escribeRespuesta(n, moneditas);
 }
--- Orden_Busqueda_tarea/A01026502_Act1.2/coinchange_funciones.h
+++ Orden_Busqueda_tarea/A01026502_Act1.2/coinchange_funciones.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <map>
 #ifndef coinchange_H
 #define coinchange_H
 
@@ -25,8 +26,7 @@ void escribeRespuesta(int n, const vector<int> &v);
 // función para imprimir el vector, recibe un vector de enteros
 // regresa un 0 si se pudo imprimir el vector
 // Complejidad O(n)
-void recibeDatos(int n, vector<int> &v)
-{
+void recibeDatos(int n, vector<int> &v){
     int x;
     for (int i = 0; i < n; i++)
     {
@@ -37,8 +37,10 @@ void recibeDatos(int n, vector<int> &v)
     return;
 }
 
-void printVector(vector<int> v)
-{
+// función para imprimir el vector, recibe un vector de enteros
+// regresa nulo y solo imprime el vector
+// Complejidad O(n)
+void printVector(vector<int> v){
     for (auto i = v.begin(); i != v.end(); ++i)
         cout << *i << " ";
     cout << endl;
@@ -48,8 +50,7 @@ void printVector(vector<int> v)
 // función para leer el archivo, recibe el nombre del archivo y un vector de enteros
 // regresa un 0 si se pudo leer el archivo
 // Complejidad O(n)
-int readFile(string x, vector<int> &v)
-{
+int readFile(string x, vector<int> &v){
     string myText;
     ifstream MyReadFile(x);
     if (MyReadFile.is_open())
@@ -88,21 +89,25 @@ int readFile(string x, vector<int> &v)
     return 0;
 }
 
-int getChange(int n, int j)
-{
+// función para calcular el cambio, recibe el pago y el costo
+// regresa el cambio
+// Complejidad O(1)
+int getChange(int n, int j){
     int c;
     // cout << "\nEl cambio es: " << endl;
     c = n - j;
     return c;
 }
 
-vector<int> minNumMonGR(int n, vector<int> &v)
-{
+// función para calcular el número mínimo de monedas con greedy, recibe el cambio y el vector de monedas
+// regresa el vector de monedas
+// Complejidad O(n)
+vector<int> minNumMonGR(int n, vector<int> &v){
     sort(v.begin(), v.end()); // ordenar el vector de menor a mayor
     vector<int> monedas;      // vector para guardar las monedas
     for (int i = v.size() - 1; i >= 0; i--)
     {
-        while (n >= v[i])
+        while (n >= v[i]) // mientras el cambio sea mayor o igual a la moneda
         {
             n = n - v[i];
             monedas.push_back(v[i]);
@@ -113,12 +118,14 @@ vector<int> minNumMonGR(int n, vector<int> &v)
     return monedas;
 }
 
-vector<int> minNumMonPD(int n, vector<int> &v)
-{
+// función para calcular el número mínimo de monedas con programación dinámica, recibe el cambio y el vector de monedas
+// regresa el vector de monedas
+// Complejidad O(n^2)
+vector<int> minNumMonPD(int n, vector<int> &v){
     sort(v.begin(), v.end());       // ordenar el vector de menor a mayor
     vector<int> monedas;            // vector para guardar las monedas
-    vector<int> dp(n + 1, INT_MAX); // INT_MAX para indicar que no hay solución
-    dp[0] = 0;
+    vector<int> dp(n + 1, INT_MAX); // INT_MAX para que cualquier valor sea menor
+    dp[0] = 0;                      // caso base
     for (int i = 1; i <= n; i++)
     {
         for (int j = 0; j < v.size() && v[j] <= i; j++)
@@ -131,14 +138,14 @@ vector<int> minNumMonPD(int n, vector<int> &v)
             }
         }
     }
-    int remaining = n;
+    int remaining = n; // variable para guardar el cambio restante
     while (remaining > 0)
     {
         for (int i = 0; i < v.size(); i++)
         {
-            if (dp[remaining] == dp[remaining - v[i]] + 1)
+            if (dp[remaining] == dp[remaining - v[i]] + 1) // si es igual al valor de un moneda
             {
-                monedas.push_back(v[i]);
+                monedas.push_back(v[i]); // agregar la moneda al vector
                 remaining = remaining - v[i];
                 break;
             }
@@ -148,12 +155,27 @@ vector<int> minNumMonPD(int n, vector<int> &v)
     return monedas;
 }
 
-void escribeRespuesta(int n, const vector<int> &v)
-{
+// función para imprimir el vector de monedas, recibe el vector de monedas
+// regresa nulo y solo imprime el vector
+// Complejidad O(n)
+void escribeRespuesta(int n, const vector<int> &v){
+    map<int, int> cuantasMon; // mapa para guardar las monedas
+    for (int coin : v)
+    { // por cada moneda en el vector
+        cuantasMon[coin]++;
+    }
     cout << "\nLas monedas son: " << endl;
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
+    for (const auto &pair : cuantasMon)
+    {                            // por cada par en el mapa
+        int count = pair.second; // guardar el valor del par
+        if (count == 1)
+        {
+            cout << "1 moneda de: " << pair.first << endl; // imprimir el valor de la moneda
+        }
+        else
+        {
+            cout << count << " monedas de: " << pair.first << endl; // imprimir el valor de la moneda
+        }
     }
     cout << endl;
 }
