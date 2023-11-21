23b4a2bbf2530cacf536fac8b0bd8d9c20e0fe11
Fernanda Osorio <A01026502@tec.mx>
2023-11-20 19:36:51 -0600

    A* start

---
+++ A01026502_Act5.5/A01026502_Act5.5.cpp
@@ -0,0 +1,14 @@
+//Fernanda Osorio 
+//A01026502
+//20 de noviembre del 2023
+//Este archivo cpp sirve para resolver un laberito con el algoritmo de A* y mostrar el camino mas corto
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+}
